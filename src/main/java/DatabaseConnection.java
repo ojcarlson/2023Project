b44a35a302b4a50b12561c9b3b8ddebf79db50c9
ocarlson1973 <ocarlson@allophone.com>
2023-04-08 10:09:03 -0500

    New Project created to integrate JavaFX into Project Spring 2023 Project

---
+++ src/main/java/DatabaseConnection.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public static Connection getConnection() {
+
+        Connection connection = null;
+        try {
+            // below two lines are used for connectivity.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/mydatabase",
+                    "root", "my-secret-pw");
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+
+        return connection;
+    }
+}
+
